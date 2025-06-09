644695b74a450732502b87d4a52e35368aa2692d
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-06-09 23:58:43 +0200

    Initial commit

---
+++ fns_and_dsa/arithmetic_operation.py
@@ -0,0 +1,18 @@
+def perform_operation():
+ #prompts the user to insert a number
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = (input('add','subtract','multiply','divide'))
+    
+    if operation == ('add'):
+       return num1 + num2
+    elif operation == ('subtract'):
+        return num1 - num2
+    elif operation == ('multiply'):
+        return num1 * num2
+    elif operation == ('divide'):
+        if num2 == 0:
+           return "Error: cannot be divided by zero"
+        return num1 / num2   
+    else:
+        return "Unknown operation"
